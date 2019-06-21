e558ee4f23e8e22b1e69028d8c68673ec90796d7
Ben Truyman <ben@truyman.com>
2019-06-21 08:28:48 -0700

    fix(yarn): ensure yarn global modules is in $PATH

--- files/lib/nodejs.zsh
+++ files/lib/nodejs.zsh
@@ -11,3 +11,6 @@ alias npmp="npm --always-auth false --registry https://registry.npmjs.com"
 npms() {
   npm search --registry=https://registry.npmjs.org "$@"
 }
+
+# yarn
+_::path::prefix "${HOME}/.config/yarn/global/node_modules/.bin"
