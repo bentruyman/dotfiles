54e345a6d79135f31d5c9db9b2e6f6f52d1021bc
Ben Truyman <ben@truyman.com>
2019-09-09 18:19:26 -0700

    feat(node): add yarn bins to $PATH

--- files/lib/nodejs.zsh
+++ files/lib/nodejs.zsh
@@ -10,3 +10,6 @@ export VOLTA_HOME="$HOME/.volta"
 [[ -s "$VOLTA_HOME/load.sh" ]] && . "$VOLTA_HOME/load.sh"
 
 _::path::prefix "${VOLTA_HOME}/bin"
+
+# yarn
+_::path::prefix "${HOME}/.config/yarn/global/node_modules/.bin"
