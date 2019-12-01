4d76723f1fccdd0cb8300f61eb91666a7103d77d
Ben Truyman <ben@truyman.com>
2019-12-01 12:34:47 -0800

    feat: add extra prettier support

--- files/lib/shortcuts.zsh
+++ files/lib/shortcuts.zsh
@@ -25,5 +25,10 @@ unalias rm 2> /dev/null
 
 alias vclean='rm "$HOME"/.local/share/nvim/swap/*'
 
+# Coding
+(( $+commands[prettier] )) && {
+  alias pretty="prettier --write '**/*.{js,json,scss,ts,vue}'"
+}
+
 # Vagrant
 alias vagrunt='vagrant destroy -f; vagrant up'
