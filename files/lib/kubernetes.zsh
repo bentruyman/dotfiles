b7673bbf51e8784ca40f65f34052c83cee6fb1fb
Ben Truyman <ben@truyman.com>
2021-02-22 09:29:38 -0800

    feat(zsh): lazy load kubectl completions

--- files/lib/kubernetes.zsh
+++ files/lib/kubernetes.zsh
@@ -1,2 +1,10 @@
 # kubectl
-# source <(kubectl completion zsh)
+_::command_exists kubectl && {
+  function kubectl() {
+    if ! type __start_kubectl >/dev/null 2>&1; then
+      source <(command kubectl completion zsh)
+    fi
+
+    command kubectl "$@"
+  }
+}
