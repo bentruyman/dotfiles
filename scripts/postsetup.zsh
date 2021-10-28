6e12c8768e1deb10977f261b9a7d88a8a053c43c
Ben Truyman <ben@truyman.com>
2021-10-28 09:58:29 -0700

    feat: install Node.js and Rust in presetup

--- scripts/postsetup.zsh
+++ scripts/postsetup.zsh
@@ -8,20 +8,6 @@ set -e
 go install golang.org/x/tools/gopls@latest
 
 ###############################################################################
-# Node.js
-###############################################################################
-
-if [[ ! -d "${HOME}/.volta/bin" ]]; then
-  curl https://get.volta.sh | bash -s -- --skip-setup
-  export PATH="${HOME}/.volta/bin:${PATH}"
-fi
-
-(( $+commands[node] )) && { volta install node; }
-(( $+commands[yarn] )) && { volta install yarn; }
-
-volta install neovim
-
-###############################################################################
 # Python/pip
 ###############################################################################
 
--- scripts/presetup.sh
+++ scripts/presetup.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-# currently a placeholder
+###############################################################################
+# Node.js
+###############################################################################
+
+if [[ ! -d "${HOME}/.volta/bin" ]]; then
+  curl https://get.volta.sh | bash -s -- --skip-setup
+fi
+
+PATH="${HOME}/.volta/bin:${PATH}"
+
+[ hash node &> /dev/null ] || volta install node
+[ hash yarn &> /dev/null ] || volta install yarn
+
+volta install neovim
+
+###############################################################################
+# Rust
+###############################################################################
+
+if [[ ! -f "${HOME}/.cargo/env" ]]; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
+fi
