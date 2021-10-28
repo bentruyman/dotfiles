033b0984ac3dba97dbc45c857c260b39184af1aa
Ben Truyman <ben@truyman.com>
2021-10-27 23:46:40 -0700

    feat(zsh): allow small letters to match capital letters in autocomplete

--- files/lib/prompt.zsh
+++ files/lib/prompt.zsh
@@ -3,6 +3,9 @@ source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/.zsh/plugins/zsh-history-substring-search/zsh-history-substring-search.zsh
 source ~/.zsh/plugins/zsh-z/zsh-z.plugin.zsh
 
+# allow small letters to match capital letters in autocomplete
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+
 autoload -U compinit && compinit
 
 eval "$(starship init zsh)"
