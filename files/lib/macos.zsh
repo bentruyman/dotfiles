5a8393440bfb40a2de0737355db06367a3b5bd60
Ben Truyman <ben@truyman.com>
2023-04-18 19:45:38 -0700

    chore: remove old brew packages

--- files/lib/macos.zsh
+++ files/lib/macos.zsh
@@ -1,10 +1,6 @@
 _::env::is_macos && {
   export BROWSER="open"
 
-  _::command_exists direnv && {
-    eval "$(direnv hook zsh)"
-  }
-
   alias brewup="brew update && brew upgrade"
   alias dsstore="find . -name \"*.DS_Store\" -type f -ls -delete"
   alias o="open ."
