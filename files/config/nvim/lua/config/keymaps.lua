acdc4ae51700376634de2e1fa6f2629275a5eb26
Ben Truyman <ben@truyman.com>
2023-08-10 22:52:08 -0700

    feat(nvim): <Leader>w for saving files (again)

--- files/config/nvim/lua/config/keymaps.lua
+++ files/config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,3 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local set_keymap = vim.api.nvim_set_keymap
+
+set_keymap("n", "<Leader>w", ":w<CR>", { noremap = true, silent = true })
