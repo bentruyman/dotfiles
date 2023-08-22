301b81a77ed5d76269bef7eb9ec4f4dccd4b542f
Ben Truyman <ben@truyman.com>
2023-08-21 19:24:06 -0700

    feat(nvim): run copilot for exercism exercises

--- files/config/nvim/lua/plugins/copilot.lua
+++ files/config/nvim/lua/plugins/copilot.lua
@@ -1,8 +1,12 @@
 return {
 	"zbirenbaum/copilot.lua",
 	cond = function()
-		-- only enable for projects on github.com
-		if vim.fn.getcwd():match("^" .. os.getenv("HOME") .. "/Development/src/github.com") then
+		-- only enable for projects on github.com or Exercism
+		local homePath = os.getenv("HOME")
+		if
+			vim.fn.getcwd():match("^" .. homePath .. "/Development/src/github.com")
+			or vim.fn.getcwd():match("^" .. homePath .. "/Exercism")
+		then
 			return true
 		end
 		return false
