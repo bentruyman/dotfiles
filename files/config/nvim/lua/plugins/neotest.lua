87ba460e9269dd5358a987d50e52870501331be8
Ben Truyman <ben@truyman.com>
2023-08-25 20:02:25 -0700

    feat(nvim): add jest support

--- files/config/nvim/lua/plugins/neotest.lua
+++ files/config/nvim/lua/plugins/neotest.lua
@@ -2,7 +2,29 @@ return {
 	{ "marilari88/neotest-vitest" },
 	{
 		"nvim-neotest/neotest",
-		opts = { adapters = { "neotest-vitest" } },
+		dependencies = {
+			"haydenmeade/neotest-jest",
+			"marilari88/neotest-vitest",
+		},
+		keys = {
+			{
+				"<leader>tw",
+				"<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
+				desc = "Run Watch",
+			},
+		},
+		opts = function(_, opts)
+			table.insert(
+				opts.adapters,
+				require("neotest-jest")({
+					jestCommand = "npm test --",
+					env = { CI = true },
+					cwd = function()
+						return vim.fn.getcwd()
+					end,
+				})
+			)
+			table.insert(opts.adapters, require("neotest-vitest"))
+		end,
 	},
-	{ "David-Kunz/jester" },
 }
