450ce8baf7cbc89538c5bd6995a4d8966ddab0b3
Ben Truyman <ben@truyman.com>
2023-09-29 13:09:43 -0700

    fix(git): ensure 'gpc' alias gets branch at execution time instead of shell login

--- files/config/fish/lib/git.fish
+++ files/config/fish/lib/git.fish
@@ -7,6 +7,7 @@ alias gdc "git diff --cached"
 alias gfr "git pull --rebase"
 alias gl "git log"
 alias gp "git push"
+alias gpc 'git push --set-upstream origin "$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"'
 alias gpf "git push --force"
 alias gs "git stash"
 alias gsp "git stash pop"
