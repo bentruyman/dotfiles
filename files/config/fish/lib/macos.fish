b29da8f3444bf1b3392e65f70c74733d8c8a521e
Ben Truyman <ben@truyman.com>
2023-09-30 00:44:10 -0700

    fix(fish): ensure platform-specific scripts are executed

--- files/config/fish/lib/macos.fish
+++ files/config/fish/lib/macos.fish
@@ -1,7 +1,5 @@
-_::env::is_macos && {
-  set -gx BROWSER "open"
+set -gx BROWSER open
 
-  alias brewup "brew update && brew upgrade"
-  alias dsstore "find . -name \"*.DS_Store\" -type f -ls -delete"
-  alias o "open ."
-}
+alias brewup "brew update && brew upgrade"
+alias dsstore "find . -name \"*.DS_Store\" -type f -ls -delete"
+alias o "open ."
--- files/config/fish/lib/ubuntu.fish
+++ files/config/fish/lib/ubuntu.fish
@@ -1,5 +1,3 @@
-_::env::is_ubuntu && {
-  fish_add_path /home/linuxbrew/.linuxbrew/bin
-  set -gx MANPATH /home/linuxbrew/.linuxbrew/share/man $MANPATH
-  set -gx INFOPATH /home/linuxbrew/.linuxbrew/share/info $INFOPATH
-}
+fish_add_path /home/linuxbrew/.linuxbrew/bin
+set -gx MANPATH /home/linuxbrew/.linuxbrew/share/man $MANPATH
+set -gx INFOPATH /home/linuxbrew/.linuxbrew/share/info $INFOPATH
