39654df1e035729069fb908dec8c09c66a2ab38d
Ben Truyman <ben@truyman.com>
2023-09-30 00:53:06 -0700

    chore(fish): remove unused config section

--- files/config/fish/config.fish
+++ files/config/fish/config.fish
@@ -1,7 +1,3 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 set fish_greeting
 set -gx DOTFILES_LIB (dirname (status --current-filename))/lib
 source $DOTFILES_LIB/init.fish
