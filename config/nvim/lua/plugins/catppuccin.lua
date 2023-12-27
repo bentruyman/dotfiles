b5c39f54b48e6156d856175298283a11d112e93e
Ben Truyman <ben@truyman.com>
2023-12-26 18:35:48 -0800

    refactor(nvim): simplify catppuccin config

--- config/nvim/lua/plugins/catppuccin.lua
+++ config/nvim/lua/plugins/catppuccin.lua
@@ -1,15 +1,5 @@
 return {
   {
-    "catppuccin/nvim",
-    priority = 1000,
-    name = "catppuccin",
-    opts = {
-      integrations = {
-        treesitter = true,
-      },
-    },
-  },
-  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
