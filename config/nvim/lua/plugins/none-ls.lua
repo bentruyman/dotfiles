d837d0e7c9a7d73a77e963644a9c7cb40dcd9b21
Ben Truyman <ben@truyman.com>
2024-01-20 15:11:33 -0800

    feat(nvim): configure biome

--- config/nvim/lua/plugins/none-ls.lua
+++ config/nvim/lua/plugins/none-ls.lua
@@ -3,7 +3,16 @@ return {
   opts = function(_, opts)
     local nls = require("null-ls").builtins
     opts.sources = {
-      nls.code_actions.gitsigns,
+      nls.formatting.biome.with({
+        args = {
+          "check",
+          "--apply-unsafe",
+          "--formatter-enabled=true",
+          "--organize-imports-enabled=true",
+          "--skip-errors",
+          "$FILENAME",
+        },
+      }),
       nls.formatting.shfmt.with({
         filetypes = { "sh", "zsh" },
       }),
