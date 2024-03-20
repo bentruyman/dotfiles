6e371b9463df4c6b06a40a35edb57d981a16e132
Ben Truyman <ben@truyman.com>
2024-03-19 21:01:34 -0700

    feat(nvim): just a bunch of things

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,3 +1,23 @@
+-- Check if the current directory has an ESLint config
+local function is_eslint_project()
+  local eslint_config_files = { ".eslintrc.js", ".eslintrc.json", "package.json" }
+  for _, file in ipairs(eslint_config_files) do
+    if vim.fn.filereadable(vim.fn.getcwd() .. "/" .. file) == 1 then
+      if file == "package.json" then
+        local package_json = vim.fn.json_decode(vim.fn.readfile("package.json"))
+        if package_json.devDependencies and package_json.devDependencies.eslint then
+          return true
+        elseif package_json.dependencies and package_json.dependencies.eslint then
+          return true
+        end
+      else
+        return true
+      end
+    end
+  end
+  return false
+end
+
 return {
   {
     "stevearc/conform.nvim",
@@ -19,11 +39,17 @@ return {
       formatters_by_ft = {
         css = { "prettierd" },
         html = { "prettierd" },
-        javascript = { "eslint_d" },
+        javascript = { "eslint_d", "prettierd" },
+        javascriptreact = { "eslint_d", "prettierd" },
         json = { "prettierd" },
         lua = { "stylua" },
-        typescript = { "eslint_d" },
-        typescriptreact = { "eslint_d" },
+        typescript = { "eslint_d", "prettierd" },
+        typescriptreact = { "eslint_d", "prettierd" },
+      },
+      formatters = {
+        eslint_d = {
+          condition = is_eslint_project,
+        },
       },
     },
     config = function(_, opts)
