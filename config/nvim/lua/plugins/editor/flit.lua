91d7d59edd88984da3b8ec7a5f77447d791b09b2
Ben Truyman <ben@truyman.com>
2025-06-06 22:01:20 -0500

    feat: support vscode-neovim

--- config/nvim/lua/plugins/editor/flit.lua
+++ config/nvim/lua/plugins/editor/flit.lua
@@ -1,5 +1,14 @@
 local flit = require("flit")
 
-flit.setup({
+-- Configure flit with VSCode-aware settings
+local config = {
   labeled_modes = "nx",
-})
+}
+
+-- Add any VSCode-specific configuration if needed
+if vim.g.vscode then
+  -- For example, you might want to adjust the keys or other settings
+  -- config.keys = { f = 'f', F = 'F', t = 't', T = 'T' }
+end
+
+flit.setup(config)
