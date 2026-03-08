e51c656cfb28266672cef549e395f4405e0aa6a5
Ben Truyman <ben@truyman.com>
2026-03-08 08:06:32 -0500

    fix(nvim): update treesitter config to new format

--- files/config/nvim/lua/plugins/editor/treesitter.lua
+++ files/config/nvim/lua/plugins/editor/treesitter.lua
@@ -1,20 +1,6 @@
-local treesitter = require("nvim-treesitter.configs")
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-parser_config.tea = {
-  install_info = {
-    url = "/Users/bentruyman/Development/src/github.com/bentruyman/tea-lang/tree-sitter-tea",
-    files = { "src/parser.c" },
-  },
-  filetype = "tea",
-}
-
 vim.opt.runtimepath:append("/Users/bentruyman/Development/src/github.com/bentruyman/tea-lang/tree-sitter-tea")
 
-treesitter.setup({
-  modules = {},
-  ignore_install = {},
-  sync_install = false,
+require("nvim-treesitter").setup({
   ensure_installed = {
     "bash",
     "diff",
@@ -32,6 +18,4 @@ treesitter.setup({
     "vimdoc",
   },
   auto_install = true,
-  highlight = { enable = true },
-  indent = { enable = true },
 })
